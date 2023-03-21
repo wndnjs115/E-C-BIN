6556f14a95e91cae2e0cf564e996e85ece6179c4
user <user@USER-PC>
2023-03-21 10:03:20 +0900

    초기 커밋.

---
+++ E-C-BIN/first-1.cpp
@@ -0,0 +1,15 @@
+/* 두개의 수의 합을 구하는 프로그램*/
+
+#include<stdio.h>
+
+int main(void)
+{
+	int x = 100; // 첫번째 정수를 저장할 변수
+	int y = 200; // 두번째 정수를 저장할 변수
+
+	int sum = x + y; // 두 정수의 합을저장하는 변수
+
+	printf("두 수의 합은? %d\n", sum);
+
+	return 0;
+}
